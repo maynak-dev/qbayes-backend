4e759b4ae308e4c9eee13c16895f44a50f74480c
maynak-dev <work.maynak@gmail.com>
2026-02-20 02:31:11 +0530

    sixth commit: backend

--- backend/settings.py
+++ backend/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
     }
 }
 
-DATABASES["default"] = dj_database_url.parse("postgresql://admindb_j2a1_user:NmxtFfTo1LaD6mD84KgODCu8kolGNWaq@dpg-d6a56lp5pdvs738ruqog-a.oregon-postgres.render.com/admindb_j2a1")
+# DATABASES["default"] = dj_database_url.parse("postgresql://admindb_j2a1_user:NmxtFfTo1LaD6mD84KgODCu8kolGNWaq@dpg-d6a56lp5pdvs738ruqog-a.oregon-postgres.render.com/admindb_j2a1")
 
 DATABASES = {
     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
