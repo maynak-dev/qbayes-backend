3737aae04593c120b5d784996ebed8b26af85cb5
maynak-dev <work.maynak@gmail.com>
2026-02-25 02:48:26 +0530

    full change backend6

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import Profile, Role, Company, Location, Shop
+from .models import *
 
 class RoleSerializer(serializers.ModelSerializer):
     company_name = serializers.CharField(source='company.name', read_only=True)
